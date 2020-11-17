75052e905d6d38f9847e38bb34902fe0fbb2eedc
Andrew Zacharia <andrew.zacharia@carterdigital.com>
2020-11-18 10:04:42 +1100

    Eslinting, tidy

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full sm:w-1/2 lg:w-1/3 p-4">
     <div
-      class="flex flex-col justify-between bg-white h-full rounded overflow-hidden shadow-sm relative min-h-128 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:border-secondary-color hover:text-secondary-color"
+      class="flex flex-col justify-between bg-white h-full rounded overflow-hidden shadow-sm relative min-h-128 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:border-secondary-color hover:text-secondary-color cursor-pointer"
     >
       <div class="px-6 py-4">
         <img :src="image" :alt="name" class="h-48 ml-auto mr-auto" />
